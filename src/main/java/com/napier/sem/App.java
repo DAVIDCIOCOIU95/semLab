1c78d20b5bb42b8dcbf0a2206ff7b54732a54681
DAVIDCIOCOIU95 <43348061+davidciocoiu95@users.noreply.github.com>
2020-01-30 23:35:57 +0000

    end of lab 3, all working

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -93,23 +93,53 @@ public class App
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT emp_no, first_name, last_name "
-                            + "FROM employees "
-                            + "WHERE emp_no = " + ID;
+                    "SELECT employees.emp_no, first_name, last_name, salary "
+                            + "FROM employees JOIN salaries ON employees.emp_no = salaries.emp_no "
+                            + "WHERE employees.emp_no = " + ID;
+
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
+
+            Employee emp = new Employee();
+
             // Return new employee if valid.
             // Check one is returned
-            if (rset.next())
-            {
-                Employee emp = new Employee();
-                emp.emp_no = rset.getInt("emp_no");
+            if (rset.next()) {
+
+                emp.emp_no = rset.getInt("employees.emp_no");
                 emp.first_name = rset.getString("first_name");
                 emp.last_name = rset.getString("last_name");
-                return emp;
-            }
-            else
+                emp.salary = rset.getInt("salary");
+            } else {
                 return null;
+            }
+
+            // Select department and manager
+            String strSelectDep = "SELECT departments.dept_name, departments.dept_no " +
+                    "FROM departments JOIN dept_emp ON departments.dept_no=dept_emp.dept_no " +
+                    "WHERE dept_emp.emp_no = " + ID;
+            rset =stmt.executeQuery(strSelectDep);
+            String deptNumb;
+            if(rset.next()) {
+                emp.dept_name = rset.getString("departments.dept_name");
+                deptNumb = rset.getString("departments.dept_no");
+            } else {
+                return null;
+            }
+
+            // Select manager
+            String strSelectManager = "SELECT first_name, last_name " +
+                    "FROM employees JOIN dept_manager ON employees.emp_no=dept_manager.emp_no " +
+                    "JOIN departments ON dept_manager.dept_no=departments.dept_no " +
+                    "WHERE departments.dept_no = " +
+                    "(SELECT dept_no " +
+                    "FROM dept_emp " +
+                    "WHERE emp_no = " + ID + ")";
+            rset = stmt.executeQuery(strSelectManager);
+            if(rset.next()) {
+                emp.manager = rset.getString("employees.first_name") + " " + rset.getString("employees.last_name");
+            }
+            return emp;
         }
         catch (Exception e)
         {
